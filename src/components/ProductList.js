70fe28b929ca7102db4e59e1dbc97940e0c0fa25
Kedar <kedarmakode1598@gmail.com>
2023-03-27 11:43:36 +0530

    add Listview for productlist component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,11 +5,19 @@ import ListView from './ListView'
 
 const ProductList = () => {
 
-  const {filtered_products: products} = useFilterContext()
+  const {filtered_products: products, grid_view} = useFilterContext()
 
-  return <GridView products={products}>
 
-  </GridView>
+  if(products.length < 1){
+    return <h5 style={{textTransform: 'none'}}>Sorry, No products match your search...</h5>
+  }
+
+  if(grid_view === false){
+    return <ListView products={products}></ListView>
+  }
+
+  return <GridView products={products}></GridView>
+  
 }
 
 export default ProductList
