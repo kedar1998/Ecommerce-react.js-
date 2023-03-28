f111d0cdf93a82fe05d2c0239287724e8d191819
Kedar <kedarmakode1598@gmail.com>
2023-03-28 10:09:49 +0530

    add authentication basic setup using Auth0

--- src/index.js
+++ src/index.js
@@ -9,14 +9,27 @@ import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+// dev-s67v108f323hfk1r.us.auth0.com - DOMAIN
+// CwFHJajyeQW07RD4beJfDUABkDqYGqBQ - CLIENT
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ProductsProvider>
-        <FilterProvider>
-            <CartProvider>
-                <App />
-            </CartProvider>
-        </FilterProvider>
-    </ProductsProvider>
+    <Auth0Provider
+    domain="dev-s67v108f323hfk1r.us.auth0.com"
+    clintId="CwFHJajyeQW07RD4beJfDUABkDqYGqBQ"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+    >
+        <UserProvider>
+            <ProductsProvider>
+                <FilterProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </FilterProvider>
+            </ProductsProvider>
+        </UserProvider>
+    </Auth0Provider>
 );
